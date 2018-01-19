663e6741a4a031aa7f103ec4fc3fd5f2f3b88fdb
Sebastiano Boem <sebastianoboem@outlook.com>
2018-01-19 13:54:29 +0100

    Aggiunto script per cambiare versione di ansible

--- oh-my-pano-zsh.zsh
+++ oh-my-pano-zsh.zsh
@@ -2,6 +2,7 @@
 # Usage: genpw 20 (without special characters) or genpw 20 char (with special characters)
 # Required pwgen, install with: brew install pwgen
 # For further help: pwgen -h
+#!/bin/bash
 genpw() {
   if [[ $2 == "char" ]]; then
     pwgen -Bcnsvy $1 1 | tr -d "\n" | pbcopy;
@@ -87,3 +88,30 @@ alias run='php artisan'
 alias build='yarn run build'
 alias start='yarn run start'
 alias build-production='yarn run build:production'
+
+# Ansible Version Changer Python Function
+function current_ansible_version {
+python - <<END
+import ansible
+print ansible.__version__
+END
+}
+
+# Display as the first line in zsh shell
+echo Current ansible version: $(current_ansible_version)
+
+# Ansible Version Changer zsh Function
+switchto(){
+  ANSV=$(current_ansible_version)
+  echo Attempt to uninstall ansible version: $ANSV...
+  pip uninstall ansible==$ANSV -q
+  wait
+  echo Successfully uninstalled ansible-$ANSV
+  echo Installing ansible version: $1
+  pip install ansible==$1 -q
+  wait
+  echo Successfully installed ansible-$1
+}
+
+# Run server trellis command
+alias provision='ansible-playbook server.yml -e env=production'
